ef5a6fb9ee3dde69ceb4b9690d381aa560901e68
Oliver Morla <73266650+OliverMorla@users.noreply.github.com>
2023-07-04 18:15:30 -0400

    fresh start with nextjs 13, react+typescript, stripe, mysql

---
+++ client/src/app/contact/page.tsx
@@ -0,0 +1,9 @@
+const Contact: React.FunctionComponent = () => {
+  return (
+    <main>
+      <h1>Contact</h1>
+    </main>
+  );
+};
+
+export default Contact;
---
+++ client/src/app/page.tsx
@@ -0,0 +1,9 @@
+const Home: React.FunctionComponent = () => {
+  return (
+    <main>
+      <h1>Home</h1>
+    </main>
+  );
+};
+
+export default Home;
